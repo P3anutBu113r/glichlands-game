dee40822517f4162cc30cc85f9dff14f67b604d7
P3anutBu113r <jonmcjon@icloud.com>
2024-08-09 14:32:06 +0800

    update

--- scenes/character.gd
+++ scenes/character.gd
@@ -1,11 +1,23 @@
-extends Node2D
+extends CharacterBody2D
 
+const speed = 100
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	pass
+	
+func player_movement(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		velocity.x = 0
+		velocity.y = -speed
+	elif Input.is_action_pressed("ui_up"):
+		velocity.x = 0
+		velocity.y = speed
+		print('hi')
+	
