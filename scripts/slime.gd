002b0ae0dc822ecb7d6ecfe1605e8fd9d3f2511a
P3anutBu113r <jonmcjon@icloud.com>
2024-08-27 07:47:25 +0800

    slime 2.0

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,4 +1,40 @@
 extends CharacterBody2D
-
+var speed = 75
+var player_chase = false
+var player = null
+@onready var anim = $AnimatedSprite2D
 func _ready():
-	$AnimatedSprite2D.play("slime idle")
+	$AnimatedSprite2D.play("slime_idle")
+func _physics_process(delta):
+	if player_chase:
+
+		position += (player.position - position).normalized() * speed * delta
+		move_and_collide(Vector2(0,0)) 
+		anim.play("slime_jump")
+		if(player.position.x - position.x) < 0:
+			anim.flip_h = true
+		else:
+			anim.flip_h = false
+	else:
+		anim.play("slime_idle")
+		
+
+#chaotic slime thats fast as f**k
+	#if player_chase:
+		#position += (player.position - position)/speed
+		#move_and_slide()
+		
+
+
+func _on_detection_area_body_entered(body):
+	if body.has_method("player"):
+		player = body
+		player_chase = true
+	
+
+
+func _on_detection_area_body_exited(body):
+	if body.has_method("player"):
+		player = null
+		player_chase = false
+	
